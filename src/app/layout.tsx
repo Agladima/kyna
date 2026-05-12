f17be374e1e19e2ef9f9b0044ea99efeb3c60c0a
Augustine Agladima <austinagladima@gmail.com>
2026-05-12 23:42:18 +0100

    repars to next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,14 +49,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className="bg-navy-900 text-cream antialiased">
         <Navbar />
         <main>{children}</main>
